43496b6967c85b0df525e8d06bd6b0ab489e4907
webpawan <pawansaini66477@gmail.com>
2023-01-15 11:30:40 +0530

    basic setup is complete

--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const path  = require("path");
 // jab ham host karte website ko to khud ka port chiya hota ha usake ya likha
-app.get("/",(req,res)=>{
-    res.send("home")
-})
+
+// public static path 
+// console.log(path.join(__dirname,"../public"));
 
 
-app.listen(port,()=>{
-console.log("start");
-})
+const static_path = path.join(__dirname,"../public");
+app.use(express.static(static_path));
+
+app.get("/", (req, res) => {
+  res.send("home");
+});
+app.get("/about", (req, res) => {
+  res.send("about");
+});
+app.get("/weather", (req, res) => {
+  res.send("weather");
+});
+app.get("*", (req, res) => {
+  res.send("error");
+});
+
+app.listen(port, () => {
+  console.log("start");
+});
